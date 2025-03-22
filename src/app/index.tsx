9507a8136c7a1ba94ef9e8ebe8d824a1481e707a
anthonyLimanto <s3898395@student.rmit.edu.au>
2025-03-23 00:06:32 +1100

    changed layout

---
+++ src/app/index.tsx
@@ -0,0 +1,35 @@
+import { Text, View } from "react-native";
+import { fetchPosts } from "../api/posts";
+import PostCard from "../components/PostCard";
+import { Post } from '../model/post';
+import { useEffect, useState } from "react";
+
+export default function Index() {
+  const [posts, setPosts] = useState<Post[]>([]);
+
+  useEffect(() => {
+    const loadPosts = async () => {
+      try {
+        const fetchedPosts = await fetchPosts(); 
+        setPosts(fetchedPosts);
+      } catch (error) {
+        console.error('Failed to fetch posts:', error);
+      }
+    };
+
+    loadPosts(); 
+  }, []);
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <Text>Edit app/index.tsx to edit this screen.</Text>
+      <Text>Below are posts</Text>
+      <PostCard post={posts[0]} />
+    </View>
+  );
+}
