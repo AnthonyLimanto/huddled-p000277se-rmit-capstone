c19816d7e360c5df9d6ea808b3d108fa2342ed7a
joviedev <sinjing81@gmail.com>
2025-03-24 17:27:06 +1100

    FullDone

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,50 @@
-import { Text, View } from "react-native";
+import { useEffect } from "react";
+import { View, Image, StyleSheet } from "react-native";
+import * as SplashScreen from "expo-splash-screen";
+import { Stack } from "expo-router";
 
-export default function Index() {
+SplashScreen.preventAutoHideAsync();
+
+const App = () => {
+  useEffect(() => {
+    async function prepare() {
+      await new Promise(resolve => setTimeout(resolve, 2000));
+      await SplashScreen.hideAsync();
+    }
+    prepare();
+  }, []);
+
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
-    </View>
+    <>
+      {/* Hides the header for this screen */}
+      <Stack.Screen options={{ headerShown: false }} />
+      
+      <View style={styles.container}>
+        <Image source={require("../assets/images/icon-only.png")} style={styles.icon} />
+        <Image source={require("../assets/images/Huddled-wordmark.png")} style={styles.wordmark} />
+      </View>
+    </>
   );
-}
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#ffffff",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  icon: {
+    width: 60,
+    height: 60,
+    marginBottom: 10,
+    resizeMode: "contain",
+  },
+  wordmark: {
+    width: 160,
+    height: 50,
+    resizeMode: "contain",
+  },
+});
+
+export default App;
