e7d832a8a44dc7c24ff53cf1c32facb43d1aadde
Anthony Limanto <s3898395@student.rmit.edu.au>
2025-04-09 11:02:25 +1000

    function to upload to bucket for Post Images

--- src/helper/bucketHelper.ts
+++ src/helper/bucketHelper.ts
@@ -1,5 +1,8 @@
 import { supabase } from "../api/supabase";
+import { decode } from 'base64-arraybuffer'
 
+
+// since all the bucket methods are similar, we should create a base function to handle the common logic, that we can reuse for all the buckets to follow the Open/Closed principle - TODO
 export const uploadPfp = async (file: File, email: string) => {
     const {data, error} = await supabase.storage
         .from("pfp")
@@ -18,6 +21,29 @@ export const downloadPfp = async (email: string) => {
 
     console.log(error)
     if (error) return "default";
+
+    const url = URL.createObjectURL(data);
+    return url;
+}
+
+export const uploadPostImage = async (base64Data: string, postId: string) => {
+    const {data, error} = await supabase.storage
+        .from("post-image")
+        .upload(postId + '/post-image.png', decode(base64Data), {
+            cacheControl: '3600',
+            upsert: true
+          });
+    if (error) throw error;
+    return data;
+}
+
+export const downloadPostImage = async (postId: string) => {
+    const {data, error} = await supabase.storage
+        .from("post-image")
+        .download(postId + '/post-image.png');
+
+    console.log(error)
+    if (error) return null;
 
     const url = URL.createObjectURL(data);
     return url;
